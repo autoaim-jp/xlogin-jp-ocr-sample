11ac38ea131311802b4d7bde549d64ebc21c3111
autoaim_jp <git@autoaim.jp>
2024-03-30 03:18:13 +0900

    refactor: refactor API endpoint paths and remove unused functions
    - Remove several unused request functions
    - Update fileAddRequest path to `/api/${API_VERSION}/tesseract/request`
    - Remove exporting of unused request functions
    
    Signed-off-by: autoaim_jp <git@autoaim.jp>

--- service/staticWeb/src/output.js
+++ service/staticWeb/src/output.js
@@ -1,70 +1,8 @@
 /* /output.js */
-
-const timerAddRequest = ({
-  accessToken, CLIENT_ID, API_VERSION, API_SERVER_ORIGIN, postRequest,
-}) => {
-  const path = `/api/${API_VERSION}/notification/append`
-  const param = {
-    notificationRange: CLIENT_ID,
-    subject: 'timer',
-    detail: 'TIME\'S UP!',
-  }
-
-  return postRequest(CLIENT_ID, accessToken, API_SERVER_ORIGIN, path, param)
-}
-
-const notificationOpenRequest = ({
-  accessToken, notificationIdList, CLIENT_ID, API_VERSION, API_SERVER_ORIGIN, postRequest,
-}) => {
-  const path = `/api/${API_VERSION}/notification/open`
-  const param = {
-    notificationRange: CLIENT_ID,
-    notificationIdList,
-  }
-
-  return postRequest(CLIENT_ID, accessToken, API_SERVER_ORIGIN, path, param)
-}
-
-const fileSaveRequest = ({
-  accessToken, message, MESSAGE_FILE_PATH, CLIENT_ID, API_VERSION, API_SERVER_ORIGIN, postRequest,
-}) => {
-  const path = `/api/${API_VERSION}/json/update`
-  const param = {
-    owner: CLIENT_ID,
-    jsonPath: MESSAGE_FILE_PATH,
-    content: message,
-  }
-
-  return postRequest(CLIENT_ID, accessToken, API_SERVER_ORIGIN, path, param)
-}
-
-const fileDeleteRequest = ({
-  accessToken, MESSAGE_FILE_PATH, CLIENT_ID, API_VERSION, API_SERVER_ORIGIN, postRequest,
-}) => {
-  const path = `/api/${API_VERSION}/json/delete`
-  const param = {
-    owner: CLIENT_ID,
-    jsonPath: MESSAGE_FILE_PATH,
-  }
-
-  return postRequest(CLIENT_ID, accessToken, API_SERVER_ORIGIN, path, param)
-}
-
-const updateBackupEmailAddressRequest = ({
-  accessToken, backupEmailAddress, CLIENT_ID, API_VERSION, API_SERVER_ORIGIN, postRequest,
-}) => {
-  const path = `/api/${API_VERSION}/user/update`
-  const param = {
-    backupEmailAddress,
-  }
-
-  return postRequest(CLIENT_ID, accessToken, API_SERVER_ORIGIN, path, param)
-}
-
 const fileAddRequest = ({
   accessToken, CLIENT_ID, API_VERSION, API_SERVER_ORIGIN, postFormRequest, formData,
 }) => {
-  const path = `/api/${API_VERSION}/file/create`
+  const path = `/api/${API_VERSION}/tesseract/request`
 
   return postFormRequest(CLIENT_ID, accessToken, API_SERVER_ORIGIN, path, formData)
 }
@@ -82,11 +20,6 @@ const endResponse = ({
 }
 
 export default {
-  timerAddRequest,
-  notificationOpenRequest,
-  fileSaveRequest,
-  fileDeleteRequest,
-  updateBackupEmailAddressRequest,
   fileAddRequest,
 
   endResponse,
